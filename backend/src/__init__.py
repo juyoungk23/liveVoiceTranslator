ab257cde7d32dfc3829379ebd39d9feb42a7b892
juyoungk23 <juyoungk23@gmail.com>
2024-05-13 13:03:10 -0700

    Update __init__.py

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,6 +1,6 @@
 # src/__init__.py
 from .audio_processing import convert_audio_to_16_bit, convert_audio_to_wav, get_audio_info
-from .secret_manager import get_secret, get_credentials
+from .secret_manager import get_secret, get_gcp_credentials
 from .transcription import transcribe_audio_whisper, transcribe_audio_google
 from .translation import translate_text
 from src.voice_generation import generate_voice_file
